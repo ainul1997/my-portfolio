f4ab585b241d32f2a43df62d3396069e3f7d1452
ainul1997 <ainul4147@gmail.com>
2025-09-15 19:02:13 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+
+
+const Home = () => {
+  return (
+    <div className="card card-side bg-base-100 shadow-sm">
+  
+  <div className="card-body">
+    <h2 className="font-semibold text-2xl"> Hello.</h2>
+    <div>
+        <p className="text-xl m-0 p-0 "> I'm Ainul</p>
+    <p className=""> Web Developer/ designer</p>
+    </div>
+    <div className="card-actions justify-start scroll-pt-16">
+      <button className="btn btn-active">Go To Project</button>
+      <button className="btn btn-active">my resume</button>
+    </div>
+  </div>
+  <figure>
+    <img className=""
+      src=""
+      alt="Movie" />
+  </figure>
+</div>
+  )
+}
+
+export default Home
